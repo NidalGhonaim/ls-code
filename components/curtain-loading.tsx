5ad5769a0d2ec94211ddd949cdb30b4084b067f8
NidalGhonaim <nidalghonaim@gmail.com>
2025-05-19 13:45:55 +0100

    add loading curtain component with spinning icon

--- components/curtain-loading.tsx
+++ components/curtain-loading.tsx
@@ -1,7 +1,12 @@
 import styles from "./curtain-loading.module.scss";
+import IconLoading from "./icon-loading";
 
 const CurtainLoading = () => {
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.container}>
+      <IconLoading />
+    </div>
+  );
 };
 
 export default CurtainLoading;
---
+++ components/icon-loading.tsx
@@ -0,0 +1,10 @@
+import { FaCircleNotch } from "react-icons/fa6";
+import styles from "./icon-loading.module.scss";
+
+const IconLoading = () => {
+  return (
+    <FaCircleNotch className={styles.container} size={64} color="white" />
+  );
+};
+
+export default IconLoading;
